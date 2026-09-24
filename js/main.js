6fb28112c356ef3552ee439a50e5de1db2d757c3
EzNiev <72237220+EzNiev@users.noreply.github.com>
2026-09-24 16:20:21 -0300

    traer tags con js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 async function cargarProyectos() {
-    const respuesta = await fetch("data/proyectos.json");
-    const proyectos = await respuesta.json();
+  const respuesta = await fetch("data/proyectos.json");
+  const proyectos = await respuesta.json();
 
-    proyectos.forEach((proyecto) => {
-        document.getElementById("projects-list").innerHTML += `
+  proyectos.forEach((proyecto) => {
+    const tagsList = proyecto.tags
+    .map((tag) => `<li>${tag}</li>`)
+    .join("")
+
+    document.getElementById("projects-list").innerHTML += `
       <article class="project-card">
         <h3 class="project-card__title">${proyecto.title}</h3>
         <p class="project-card__description">${proyecto.description}</p>
+        <ul class="skills">
+          ${tagsList}
+        </ul>
       </article>
     `;
-    });
+  });
 }
 
 cargarProyectos();
